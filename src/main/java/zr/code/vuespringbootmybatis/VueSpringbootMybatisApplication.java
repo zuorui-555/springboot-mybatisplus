d6da55c74db14d481b9496e5f7445779ed00ae3b
Zuorui <zuorui.work@outlook.com>
2022-05-19 21:44:20 +0800

    test

---
+++ src/main/java/zr/code/vuespringbootmybatis/VueSpringbootMybatisApplication.java
@@ -0,0 +1,13 @@
+package zr.code.vuespringbootmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringbootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueSpringbootMybatisApplication.class, args);
+    }
+
+}
---
+++ src/main/java/zr/code/vuespringbootmybatis/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package zr.code.vuespringbootmybatis.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import zr.code.vuespringbootmybatis.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
