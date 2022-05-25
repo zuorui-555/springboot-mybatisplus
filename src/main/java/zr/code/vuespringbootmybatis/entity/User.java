70f45fadd00fffb917ca06885d289b8e4110df5a
Zuorui <zuorui.work@outlook.com>
2022-05-25 22:04:12 +0800

    data

--- src/main/java/zr/code/vuespringbootmybatis/entity/User.java
+++ src/main/java/zr/code/vuespringbootmybatis/entity/User.java
@@ -3,7 +3,6 @@ package zr.code.vuespringbootmybatis.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @TableName("user")
