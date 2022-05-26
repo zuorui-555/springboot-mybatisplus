36e99987bc7870baf802e00ccc01c8a6edce8d3d
Zuorui <zuorui.work@outlook.com>
2022-05-27 00:13:41 +0800

    vue

---
+++ web/vue/src/components/Aside.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-menu
+        style = "width: 200px; min-height: calc(100vh - 50px)"
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+    >
+      <el-sub-menu index="1">
+        <template #title>
+          <span>Navigator One</span>
+        </template>
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item one</el-menu-item>
+      </el-sub-menu>
+      <el-menu-item index="2">
+        <el-icon><icon-menu /></el-icon>
+        <span>Navigator Two</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <el-icon><setting /></el-icon>
+        <span>Navigator Four</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Asider"
+}
+</script>
+
+<style scoped>
+
+</style>
