e1e6bde07409331eb9a38f62ea3f32ad73cce272
Zuorui <zuorui.work@outlook.com>
2022-06-13 11:37:27 +0800

    vue

---
+++ web/vue/src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Layout from '../layout/Layout.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Layout',
+    component: Layout,
+    redirect: "/home",
+    children: [
+      {
+        path: 'home',
+        name: 'Home',
+        component: () => import("@/views/Home")
+      }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import("@/views/Login")
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import("@/views/Register")
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
